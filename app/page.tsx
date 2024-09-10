c1c1be8078a9132639975e7401db5cddc8d20acf
Franck <franck.fernandez@gmail.com>
2024-09-10 19:30:17 +0200

    Create form

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,11 @@ export default async function Home() {
   const expenses = await fetchQuery(api.expenses.get);
   return (
     <main className="max-w-5xl mx-auto mt-5">
-      <ExpenseTable className="" expenses={expenses} />
+      {expenses ? (
+        <ExpenseTable className="" expenses={expenses} />
+      ) : (
+        'No expenses'
+      )}
     </main>
   );
 }
--- components/ExpenseModal.tsx
+++ components/ExpenseModal.tsx
@@ -3,14 +3,23 @@ import {
   AlertDialogAction,
   AlertDialogCancel,
   AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
   AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
 import { Button } from '@/components/ui/button';
 import { Plus } from 'lucide-react';
+import { Label } from './ui/label';
+import { Input } from './ui/input';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from './ui/select';
+
+const TAGS = ['insurance', 'rent', 'bank'];
 
 export function ExpenseModal() {
   return (
@@ -23,18 +32,41 @@ export function ExpenseModal() {
 
       <AlertDialogContent>
         <AlertDialogHeader>
-          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-
-          <AlertDialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </AlertDialogDescription>
+          <AlertDialogTitle>Add expense</AlertDialogTitle>
         </AlertDialogHeader>
 
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
-        </AlertDialogFooter>
+        <form action="" className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="title">Title</Label>
+            <Input id="title" type="text" placeholder="Netflix" required />
+          </div>
+
+          <div className="grid gap-2">
+            <Label htmlFor="title">Tags</Label>
+            <Select required>
+              <SelectTrigger className="">
+                <SelectValue placeholder="-" />
+              </SelectTrigger>
+              <SelectContent>
+                {TAGS.map((tag) => (
+                  <SelectItem className="capitalize" value={tag} key={tag}>
+                    {tag}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+
+          <div className="grid gap-2">
+            <Label htmlFor="amount">Amount</Label>
+            <Input id="amount" type="number" placeholder="19.99" required />
+          </div>
+
+          <div className="flex gap-3 justify-end mt-3">
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction type="submit">Add</AlertDialogAction>
+          </div>
+        </form>
       </AlertDialogContent>
     </AlertDialog>
   );
