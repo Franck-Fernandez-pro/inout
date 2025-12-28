f5f6a55e755e559d05999d80836441a8567402e2
Franck FERNANDEZ <ffernandez.externe@bedrockstreaming.com>
2025-12-28 21:57:11 +0100

    Init convex

---
+++ convex/schema.ts
@@ -0,0 +1,17 @@
+import { defineSchema, defineTable } from 'convex/server';
+import { v } from 'convex/values';
+
+export const transactions = {
+  title: v.string(),
+  tags: v.string(),
+  amount: v.string(),
+  type: v.union(v.literal('IN'), v.literal('OUT')),
+  userId: v.id('users'),
+};
+
+export default defineSchema({
+  transactions: defineTable(transactions).index('by_user_type', [
+    'userId',
+    'type',
+  ]),
+});
