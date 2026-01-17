7ad20c397a23532e2eba462038d5289eaac936ac
Franck FERNANDEZ <ffernandez.externe@bedrockstreaming.com>
2026-01-17 13:21:36 +0100

    Install auth config and create provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,31 @@
-import { ConvexProvider, ConvexReactClient } from 'convex/react';
+import { ClerkProvider, useAuth } from '@clerk/clerk-expo';
+import { ConvexReactClient } from 'convex/react';
+import { ConvexProviderWithClerk } from 'convex/react-clerk';
 import { Stack } from 'expo-router';
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
 
 if (!process.env.EXPO_PUBLIC_CONVEX_URL) {
   throw new Error('EXPO_PUBLIC_CONVEX_URL environment variable is not set');
 }
 
-const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
-  unsavedChangesWarning: false,
-});
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!);
 
 export default function RootLayout() {
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+  if (!publishableKey) {
+    throw new Error(
+      'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env'
+    );
+  }
+
   return (
-    <ConvexProvider client={convex}>
-      <Stack>
-        <Stack.Screen name="index" />
-      </Stack>
-    </ConvexProvider>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <Stack>
+          <Stack.Screen name="index" />
+        </Stack>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
   );
 }
