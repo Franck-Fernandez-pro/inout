f48c4b696b6e18ae6a5c2e56767a8b42a4b36860
Franck FERNANDEZ <ffernandez.externe@bedrockstreaming.com>
2026-01-23 17:45:33 +0100

    Add google login

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,13 @@
 import { api } from '@/convex/_generated/api';
+import { useAuth, useSSO } from '@clerk/clerk-expo';
 import { useQuery } from 'convex/react';
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+import * as Linking from 'expo-linking';
 
 export default function Index() {
+  const { signOut, isSignedIn } = useAuth();
+  const { startSSOFlow } = useSSO();
+
   const transactionIns = useQuery(api.transactions.get, {
     type: 'IN',
     userId: 'toto',
@@ -12,24 +17,43 @@ export default function Index() {
     userId: 'toto',
   });
 
+  const handleLogIn = async () => {
+    try {
+      const { createdSessionId, setActive } = await startSSOFlow({
+        redirectUrl: Linking.createURL('/dashboard', { scheme: 'inout' }),
+        strategy: 'oauth_google',
+      });
+
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId });
+      } else {
+        // 2FA
+      }
+    } catch (err) {
+      console.error('Erreur OAuth', err);
+    }
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>
-        {transactionIns?.length
-          ? transactionIns.map((transaction) => transaction.title)
-          : null}
-      </Text>
-      <Text>
-        {transactionOuts?.length
-          ? transactionOuts.map((transaction) => transaction.title)
-          : null}
-      </Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {isSignedIn ? (
+        <>
+          <Text>Connecté !</Text>
+          <Text>
+            {transactionIns?.length
+              ? transactionIns.map((transaction) => transaction.title)
+              : null}
+          </Text>
+          <Text>
+            {transactionOuts?.length
+              ? transactionOuts.map((transaction) => transaction.title)
+              : null}
+          </Text>
+          <Button title="Se déconnecter" onPress={() => signOut()} />
+        </>
+      ) : (
+        <Button title="Se connecter avec Google" onPress={handleLogIn} />
+      )}
     </View>
   );
 }
